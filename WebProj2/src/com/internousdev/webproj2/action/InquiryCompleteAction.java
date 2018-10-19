172ba0e8ec1f8694df11a4dd4791bb837500ea67
ikeyoshi <yoshpond0147@gmail.com>
2018-10-19 21:50:22 +0900

    3-20

--- WebProj2/src/com/internousdev/webproj2/action/InquiryCompleteAction.java
+++ WebProj2/src/com/internousdev/webproj2/action/InquiryCompleteAction.java
@@ -13,6 +13,32 @@ public class InquiryCompleteAction extends ActionSupport{
 		 String ret=ERROR;
 		 InquiryCompleteDAO dao=new InquiryCompleteDAO();
 		 int count=dao.insert(name, qtype, body);
+		 if(count  > 0) {
+	     ret=SUCCESS;
+		 }
+         return ret;
+	 }
+	 public String getName() {
+		  return name;
+	 }
 
+	 public void setName(String name) {
+		  this.name = name;
+	 }
+
+	 public String getQtype() {
+		  return qtype;
+	 }
+
+	 public void setQtype(String qtype) {
+		  this.qtype = qtype;
+	 }
+
+	 public String getBody() {
+		  return body;
+	 }
+
+	 public void setBody(String body) {
+		  this.body = body;
 	 }
 }
