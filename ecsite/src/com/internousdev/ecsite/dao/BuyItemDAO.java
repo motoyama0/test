99c596823fa0ae3cde2a723ed28cfde54adcb02a
maifujii <b1100136@gl.aiu.ac.jp>
2018-11-28 21:31:04 +0900

    6

--- ecsite/src/com/internousdev/ecsite/dao/BuyItemDAO.java
+++ ecsite/src/com/internousdev/ecsite/dao/BuyItemDAO.java
@@ -1,5 +1,4 @@
 package com.internousdev.ecsite.dao;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,30 +7,30 @@ import com.internousdev.ecsite.dto.BuyItemDTO;
 import com.internousdev.ecsite.util.DBConnector;
 
 public class BuyItemDAO {
+         private DBConnector dbConnector = new DBConnector();
+         private Connection connection = dbConnector.getConnection();
+         private BuyItemDTO buyItemDTO = new BuyItemDTO();
 
-	private DBConnector dbConnector = new DBConnector();
-    private Connection connection = dbConnector.getConnection();
-    private BuyItemDTO buyItemDTO = new BuyItemDTO();
+	     public BuyItemDTO getBuyItemInfo() {
+	    	      String sql = "SELECT id, item_name,item_price FROM item_info_transaction";
 
-    public BuyItemDTO getBuyItemInfo() {
-    	    String sql = "SELECT id,item_name,item_price FROM item_info_transaction";
+	    	      try {
+	    	    	  	  PreparedStatement preparedStatement = connection.prepareStatement(sql);
+	    	    	  	  ResultSet resultSet = preparedStatement.executeQuery();
 
-    	    try {
-    	    	    PreparedStatement preparedStatement = connection.prepareStatement(sql);
-    	    	    ResultSet resultSet = preparedStatement.executeQuery();
-    	    	    if(resultSet.next()) {
-    	    	    	     buyItemDTO.setId(resultSet.getInt("id"));
-    	    	    	     buyItemDTO.setItemName(resultSet.getString("item_name"));
-    	    	    	     buyItemDTO.setItemPrice(resultSet.getString("item_price"));
-    	    	    }
-    	    } catch(Exception e) {
-    	    	    e.printStackTrace();
-    	    }
+	    	    	  	  if(resultSet.next()) {
+	    	    	  		       buyItemDTO.setId(resultSet.getInt("id"));
+	    	    	  		       buyItemDTO.setItemName(resultSet.getString("item_name"));
+	    	    	  		       buyItemDTO.setItemPrice(resultSet.getString("item_price"));
+	    	    	  	  }
+	    	      } catch(Exception e) {
+	    	    	       e.printStackTrace();
+	    	      }
 
-    	    return buyItemDTO;
-    }
+	    	      return buyItemDTO;
+	     }
 
-    public BuyItemDTO getBuyItemDTO() {
-    	    return buyItemDTO;
-    }
+	     public BuyItemDTO getBuyItemDTO() {
+	    	     return buyItemDTO;
+	     }
 }
